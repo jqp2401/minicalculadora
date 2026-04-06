d4c252b89cfc662260ad5e7c8ad1df7b9e291028
jqp2401 <jqueper2401@g.educaand.es>
2026-04-06 12:43:59 +0200

    Utlimos cambios de hora finalizada

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        System.out.println(calculadora.valorAbsoluto(5));
-        System.out.println(calculadora.raizCuadrada(5));
+        System.out.printf("Valor absoluto: %.2f%n", calculadora.valorAbsoluto(-8));
+        System.out.printf("Raiz cuadrada: %.2f%n", calculadora.raizCuadrada(5));
     }
 }
