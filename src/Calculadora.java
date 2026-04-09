b3e79b15fbcc3eff49c7b0abd0d5d51fa8c0f92d
jqp2401 <jqueper2401@g.educaand.es>
2026-04-09 13:01:59 +0200

    Documentacion metodo 'raizCuadrada'

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -14,6 +14,12 @@ public class Calculadora {
         return valAbs;
     }
 
+    /**
+     * Metodo que pasando un numero devuelve la raiz cuadrada
+     * @author jqp2401
+     * @param numero double
+     * @return Double del valor del resultado de la raiz cuadrada.
+     */
     public double raizCuadrada (double numero){
         double raizC = numero;
         raizC = Math.sqrt(raizC);
